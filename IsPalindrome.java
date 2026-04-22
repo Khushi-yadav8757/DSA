942c17fd5432d147d33dde928da92fa41bb95386
Khushi Yadav <kky957242@gmail.com>
2026-04-22 20:45:46 +0530

    Update IsPalindrome.java

--- IsPalindrome.java
+++ IsPalindrome.java
@@ -5,7 +5,6 @@ public class PalindromeChecker {
         // Remove non-alphanumeric characters and convert to lowercase
         String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int left = 0, right = cleaned.length() - 1;
-
         while (left < right) {
             if (cleaned.charAt(left) != cleaned.charAt(right)) {
                 return false;
