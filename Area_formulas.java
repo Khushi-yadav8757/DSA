1076424628c744562b93e161339328ed58f56d4a
Khushi Yadav <kky957242@gmail.com>
2026-04-28 15:44:18 +0530

    Update Area_formulas.java

--- Area_formulas.java
+++ Area_formulas.java
@@ -1,9 +1,10 @@
 public class FormulaExamples {
    public static void main(String[] args) {
+      
         double radius = 7;
         int length = 10, width = 5;
         double base = 6, height = 4;
-
+      
         double areaCircle = Math.PI * radius * radius;
         int areaRectangle = length * width;
         double areaTriangle = 0.5 * base * height;
