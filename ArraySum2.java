02eecdc3e1b4c5aa36f20bf7a8325dfd04121a70
Khushi Yadav <kky957242@gmail.com>
2026-04-29 09:11:29 +0530

    Update ArraySum2.java

--- ArraySum2.java
+++ ArraySum2.java
@@ -2,7 +2,6 @@ class Main {
     public static void main(String[] args) {
         int [] arr = {10, 20, 30};
         int sum = 0;
-
         for(int i = 0; i < arr.length; i++){
             sum = sum + arr[i];
         }
