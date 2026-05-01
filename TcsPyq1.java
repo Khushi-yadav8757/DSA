61fdf9cf3192e60826dd8c5ff9f3a5b5cf6221bc
Khushi Yadav <kky957242@gmail.com>
2026-05-01 14:32:38 +0530

    Update TcsPyq1.java

--- TcsPyq1.java
+++ TcsPyq1.java
@@ -1,8 +1,6 @@
 /*1️. Move all zeros to the end of the array
-
 Problem:
 You are given an integer array arr of size N. Move all the zeros in the array to the end while maintaining the relative order of the non-zero elements.
-
 Example:
 Input: arr = [4, 5, 0, 1, 9, 0, 5, 0]
 Output: [4, 5, 1, 9, 5, 0, 0, 0] */
